5dc4235914eef72f47433e9c9584e80917576ce9
tikho <iatikho@gmail.com>
2017-01-07 17:59:42 +0300

    heroku-server reworked

--- heroku-server.py
+++ heroku-server.py
@@ -1,5 +1,8 @@
+import os
 from os import environ
 from flask import Flask
 
 app = Flask(__name__)
-app.run(environ.get('PORT'))
+# Bind to PORT if defined, otherwise default to 5000.
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
